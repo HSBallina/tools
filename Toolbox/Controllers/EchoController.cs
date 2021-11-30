0dea231b60d6d09c4969424f12ad3bf2cfd6dd2e
HSBallina <hakan.sjo@valtech.com>
2021-11-30 12:52:27 +0100

    Here we go

---
+++ Toolbox/Controllers/EchoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toolbox.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new OkObjectResult("Nothing here");
+        }
+
+        [HttpPost]
+        public IActionResult Echo([FromBody] dynamic content)
+        {
+            return new OkObjectResult(JsonSerializer.Serialize(content));
+        }
+    }
+
+    public class Pop
+    {
+        public string Yo { get; set; }
+    }
+}
---
+++ Toolbox/Controllers/EncryptController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toolbox.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EncryptController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string s = null)
+        {
+            return s == null ? "Usage: encrypt?s=[string to sign]" : GenerateSign(s);
+        }
+
+        private static string GenerateSign(string signString)
+        {
+            using var sha384Hash = SHA384.Create();
+            
+            var sourceBytes = Encoding.UTF8.GetBytes(signString);
+            var hashBytes = sha384Hash.ComputeHash(sourceBytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", "");
+
+            return hash.ToLower();
+        }
+
+    }
+}
